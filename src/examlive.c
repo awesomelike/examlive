bd167e69cbb90f2f8a00d3fe5544b953cab162d0
awesomelike <mr.azik98@mail.ru>
2019-11-15 23:18:29 +0500

    merged Saidakbar's awesome GUI work with the main file

--- src/examlive.c
+++ src/examlive.c
@@ -1,13 +1,18 @@
 #include<mysql/mysql.h>
+#include<signal.h>
+#include <sys/types.h>
+#include<unistd.h>
 #include<gtk/gtk.h>
+#include<gtk/gtkx.h>
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+#include <ctype.h>
 
-//GTK components
-
-
-
+//CSS values
+GtkCssProvider  *provider;
+GdkScreen       *screen;
+GdkDisplay      *display;
 
 //Database credentials
 static char *host = "db4free.net";
@@ -18,15 +23,26 @@ unsigned int port = 3306;
 static char* unix_socket = NULL;
 unsigned int flag = 0;
 
+
+//GTK Login Page
+GtkWidget	*login_window;
+GtkWidget	*login_fixed;
+GtkWidget	*login_image_1;
+GtkWidget	*login_button;
+GtkWidget	*login_username;
+GtkWidget	*login_password;
+GtkBuilder	*builder; 
+
+
 //This function establishes DB connection
-void db_connect (MYSQL *conn) {
-	conn = mysql_init(NULL);
-	if(!(mysql_real_connect(conn, host, user, pass, dbname, port, unix_socket, flag))) {
-		printf("%s\n", "some error there gentleman");
-		return;
-	}
-	printf("%s\n", "DB connection successful!");
-}
+// void db_connect (MYSQL *conn) {
+// 	conn = mysql_init(NULL);
+// 	if(!(mysql_real_connect(conn, host, user, pass, dbname, port, unix_socket, flag))) {
+// 		printf("%s\n", "some error there gentleman");
+// 		return;
+// 	}
+// 	printf("%s\n", "DB connection successful!");
+// }
 
 int main(int argc, char *argv[]) {
 
@@ -34,28 +50,41 @@ int main(int argc, char *argv[]) {
 	MYSQL *conn;
 	conn = mysql_init(NULL);
 	if(!(mysql_real_connect(conn, host, user, pass, dbname, port, unix_socket, flag))) {
-		printf("%s\n", "some error there gentleman");
-		return;
-	}
-	//printf("%s\n", "DB connection successful!");
-	
-	
-	
-	if (mysql_query(conn, "SELECT * FROM courses")) {
-	 	fprintf(stderr, "Error=%s\n", mysql_error(conn));
-	}
-	MYSQL_RES *res;
-	res = mysql_store_result(conn);
-	int num_fields = mysql_num_fields(res);
-	MYSQL_ROW *row;
-	//printf("%d\n", num_fields);
-	while ((row = mysql_fetch_row(res)))
-	{
-		for(int i=0; i<num_fields; i++) {
-			printf("%s ", row[i]);
-		}
+		printf("%s\n", "Database connection error!");
+		return 0;
 	}
 	
-	
+
+	gtk_init(&argc, &argv); 
+ 
+	builder = gtk_builder_new_from_file ("ui/test.glade");
+ 
+	login_window = GTK_WIDGET(gtk_builder_get_object(builder, "login_window"));
+
+	g_signal_connect(login_window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+
+    gtk_builder_connect_signals(builder, NULL);
+
+    //declare variables
+	login_fixed = GTK_WIDGET(gtk_builder_get_object(builder, "login_fixed"));
+	login_button = GTK_WIDGET(gtk_builder_get_object(builder, "login_button"));
+	login_image_1 = GTK_WIDGET(gtk_builder_get_object(builder, "login_image_1"));
+	login_username = GTK_WIDGET(gtk_builder_get_object(builder, "login_username"));
+	login_password = GTK_WIDGET(gtk_builder_get_object(builder, "login_password"));
+
+	//These lines used to connect CSS
+    provider = gtk_css_provider_new();
+    display = gdk_display_get_default();
+    screen = gdk_display_get_default_screen (display);
+    gtk_style_context_add_provider_for_screen (screen, GTK_STYLE_PROVIDER(provider), GTK_STYLE_PROVIDER_PRIORITY_USER);
+    gtk_css_provider_load_from_path(GTK_CSS_PROVIDER(provider),"style/style.css",NULL);
+
+   
+
+	gtk_widget_show(login_window);
+
+
+	gtk_main();
+		
 	return 0;
 }
